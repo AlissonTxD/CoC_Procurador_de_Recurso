dc5957eab5b4890210bb8d9adcaa46d85ac1cf3c
Alisson Notebook <omigadortxd@gmail.com>
2025-03-28 06:28:51 -0400

    feat: implement multithreading for village search and enhance error handling in SearchModel

--- src/controllers/controller_search.py
+++ src/controllers/controller_search.py
@@ -1,18 +1,44 @@
+from PyQt5.QtCore import QThread
+
 from src.views.view_main import MainGuiView
 from src.models.model_image_generator import ImageGerenatorModel
 from src.models.model_ocr_reading import SearchModel
 
+thread = None
+village_searcher = None
 
 def search_village():
+    global thread, village_searcher
     view = MainGuiView() 
     view.btn_stop.setEnabled(True)
     view.btn_search.setEnabled(False)
     view.tooltip("Buscando Vilas com Recursos")
+    
     img_generator = ImageGerenatorModel()
     village_searcher = SearchModel(view.ocr, img_generator, view.minimum)
-    village_searcher.run()
+    thread = QThread()
+    village_searcher.moveToThread(thread)
+    thread.started.connect(village_searcher.run)
+    village_searcher.error.connect(show_error)
+    village_searcher.finished.connect(thread.quit)
+    village_searcher.finished.connect(village_searcher.deleteLater)
+    village_searcher.finished.connect(thread.deleteLater)
+    village_searcher.finished.connect(stop_searching)
+    thread.start()
+    print("thread started")
     
-  
-
 def stop_searching():
-    print("botao parar apertado")
+    global thread, village_searcher
+
+    if thread and thread.isRunning():
+        thread.quit()
+        thread.wait(500)
+    view = MainGuiView()
+    view.tooltip()
+    view.btn_stop.setEnabled(False)
+    view.btn_search.setEnabled(True)
+    thread = None
+    village_searcher = None
+
+def show_error(error_msg: str):
+    print(error_msg)
--- src/models/model_ocr_reading.py
+++ src/models/model_ocr_reading.py
@@ -1,25 +1,61 @@
-from PyQt5.QtCore import QObject, pyqtSignal
+from collections import namedtuple
+from time import sleep
 
-from pyautogui import pixelMatchesColor, click
+from PyQt5.QtCore import QObject, pyqtSignal
+from pyautogui import pixelMatchesColor, click, pixel
+from playsound import playsound
 
 from src.models.model_image_generator import ImageGerenatorModel
+
+Coord = namedtuple("Coord", "x y")
+TARGET_PIXEL = Coord(1679, 760)  # NOT
+#TARGET_PIXEL = Coord(1644, 733)  # PC
+RGB = (255, 255, 255)
+RGB2 = (224, 224, 224)
 
 GOLD_IMAGE_PATH = "temp/gold.png"
 ELIXIR_IMAGE_PATH = "temp/elixir.png"
 DARK_IMAGE_PATH = "temp/dark.png"
+MP3_PATH = "src/midia/mp3/vila.mp3"
 
-class SearchModel:
+class SearchModel(QObject):
+    finished = pyqtSignal()
+    error = pyqtSignal(str)
+
     def __init__(self, ocr, image_generator: ImageGerenatorModel, minimum):
+        super().__init__()
         self.image_generator = image_generator
         self.ocr = ocr
         self.minimum = minimum
 
     def run(self):
-        self.__format_minimum(self.minimum)
-        self.image_generator.generate_image()
-        self.read_response = self.__format_response_ocr()
-        print(self.minimum)
-        print(self.read_response)
+        print("run running")
+        try:
+            self.__format_minimum(self.minimum)
+            #foca na janela
+            sleep(1)
+            while True:
+                while True:
+                    if self.__village_is_loaded():
+                        print("Vila carregada")
+                        break
+                    sleep(1)
+                    pix = pixel(TARGET_PIXEL.x, TARGET_PIXEL.y)
+                    print(f"Aguardando vila ser carregada: {pix}")
+                self.image_generator.generate_image()
+                village_resources = self.__format_response_ocr()
+                print(self.minimum)
+                print(village_resources)
+                if self.__village_is_fat(self.minimum, village_resources):
+                    print("Vila gorda encontrada")
+                    playsound(MP3_PATH, block=False)
+                    break
+                else:
+                    click(x= TARGET_PIXEL.x, y= TARGET_PIXEL.y)
+                    sleep(2)
+        except Exception as e:
+                self.error.emit(f"Erro: {e}")
+        self.finished.emit()
 
     def __format_response_ocr(self):
         gold_in_village = self.__read_img_ocr(GOLD_IMAGE_PATH)
@@ -38,7 +74,6 @@ class SearchModel:
                 new_list.append(0)
         self.minimum = tuple(new_list)
         
-
     def __read_img_ocr(self, path):
         try:
             img_path = path
@@ -56,3 +91,28 @@ class SearchModel:
             return textolido
         except:
             return 0
+
+    def __village_is_loaded(self):
+        test1 = pixelMatchesColor(
+                    TARGET_PIXEL.x,
+                    TARGET_PIXEL.y,
+                    RGB)
+        test2 = pixelMatchesColor(
+                TARGET_PIXEL.x,
+                TARGET_PIXEL.y,
+                RGB2)
+        if test1 or test2:
+            return True
+        else: 
+            return False
+                
+    def __village_is_fat(self, minimum: tuple, village_resources: tuple) -> None:
+        """"Checks if the village has the minimum resources.
+        Args:
+            minimum (tuple): minimum resources to check.
+            village_resources (tuple): resources in the actual village."""
+        for min, resource in zip (minimum, village_resources):
+            if min != 0 and resource < min:
+                return False
+        return True
+
