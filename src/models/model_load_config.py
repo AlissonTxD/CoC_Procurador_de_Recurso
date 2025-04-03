a6ce1a0fa9e51802b0bd1cf760b9dc7f3c8635a5
Alisson Notebook <omigadortxd@gmail.com>
2025-04-02 23:16:41 -0400

    feat: implement ConfigurationRepository for loading and managing JSON configuration data

---
+++ src/models/model_load_config.py
@@ -0,0 +1,33 @@
+#TODO model que ira ler a config do json e utilizara nas outras classes
+import json
+from typing import List, Dict
+
+PATH = "config.json"
+
+
+class ConfigurationRepository:
+    def __init__(self) -> None:
+        self.configurations = []
+
+    def get_json_data(self, path: str= PATH) -> List:
+        self.__load_data(path)
+        return self.configurations
+
+    def __load_data(self, path: str) -> None:
+        try:
+            self. configurations = self.__open_json(path)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Aquivo de configuração não encontrado: {path}")
+
+    def __open_json(self, path: str) -> list:
+        with open(path, "r") as fp:
+            var = json.load(fp)
+            return var
+
+if __name__ == "__main__":
+    repository = ConfigurationRepository()
+    data = repository.get_json_data()
+    #print(data)
+    for recurso in data["generator"]:
+        for value in data["generator"][recurso]:
+            print(value)
